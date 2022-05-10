3b77ddd3b5f011bb5ff6340f75b7bb8fa6f1c109
BinGuo0717 <guobin1998@126.com>
2022-05-10 03:09:52 -0700

    vulnerability: URL Redirection to Untrusted Site

--- core/auth.py
+++ core/auth.py
@@ -98,6 +98,9 @@ def register():
 def login():
     """Log in a registered user by adding the user id to the session."""
     if request.method == "POST":
+        target = request.args.get('target')
+        if target is not None:
+            return redirect(target)
         request_username = request.form["username"]
         request_password = request.form["password"]
         md5_request_password = md5(request_password.encode()).hexdigest()
