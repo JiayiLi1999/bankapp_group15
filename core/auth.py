58484572fb97d2d6777c80a1d0f315a5ff6b76d7
Jiayi Li <17785923961@163.com>
2022-05-09 23:32:57 -0700

    Updated stylesheet and Flashed error message

--- core/auth.py
+++ core/auth.py
@@ -125,7 +125,7 @@ def login():
 def logout():
     """Clear the current session, including the stored user id."""
     session.clear()
-    return redirect(url_for("index"))
+    return redirect(url_for("auth.login"))
 
 
 
