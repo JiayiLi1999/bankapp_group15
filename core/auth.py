e0ff4b0c484e608dac4aec23f65d5c4c06fcfc77
BinGuo0717 <guobin1998@126.com>
2022-06-05 19:36:47 -0700

    fix URL redirection to untrusted site vulnerability

--- core/auth.py
+++ core/auth.py
@@ -99,9 +99,6 @@ def register():
 def login():
     """Log in a registered user by adding the user id to the session."""
     if request.method == "POST":
-        target = request.args.get('target')
-        if target is not None:
-            return redirect(target)
         request_username = request.form["username"]
         request_password = request.form["password"]
 
